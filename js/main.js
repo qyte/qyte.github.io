dac0a2213243772373ad2cd21ad4fb3c0f022c2f
qyte <jimgdews@gmail.com>
2014-06-26 18:34:37 +0300

    asd

--- js/main.js
+++ js/main.js
@@ -281,7 +281,7 @@ function drawAgents(){
 }
 
 function drawFog(area){
-    ctx.fillStyle = 'rgba(0,0,0,0.5)';
+    ctx.fillStyle = 'rgba(0,0,0,0.2)';
     for (var i = 0; i < area.length; i++)
         for (var j = 0; j < area[i].length; j++)
             if(!area[i][j])
